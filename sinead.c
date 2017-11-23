e8879f838b339ba83dc7c7c9d2468927fd8a5a8c
Sinead54 <33353611+Sinead54@users.noreply.github.com>
2017-11-23 13:41:52 +0000

    Update sinead.c

--- sinead.c
+++ sinead.c
@@ -1,1 +1,17 @@
-hi
+#include<stdio.h>
+
+int main () {
+  double coeff[5]= {2,5,7,5,4}; //n is an array of integers
+  int i,j;
+  int maxValue = coeff[0];
+  for (i=0;i<=5;i++){
+    if (coeff[i]>maxValue){
+      maxValue= coeff[i];	
+    }
+  }
+  for (j=0;j<=5;j++){
+    coeff[j]=coeff[j]/maxValue;
+  }
+
+}
+
