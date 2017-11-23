91e0bd9bafd9fe4b6c1c55ce7e50669565bc7123
Sinead54 <33353611+Sinead54@users.noreply.github.com>
2017-11-23 16:32:29 +0000

    Update sinead.c

--- sinead.c
+++ sinead.c
@@ -2,23 +2,41 @@
 
 int main () {
   double coeff[5]= {2,5,7,5,4}; //n is an array of integers
-  int iNorm,jNorm;
-  int maxValue = coeff[0];
+  int iNorm,jNorm;//varibles forthe for loop
+  int maxValue = coeff[0]; //assign the forst value in the array to the max value
+  //The below for loop starts at the first value checks if it is larger 
+  //than the current max value if it is it rassigns this value to 
+  //maxValue other wise maxValue remains unchanged.
   for (iNorm=0;iNorm<=5;iNorm++){
     if (coeff[iNorm]>maxValue){
       maxValue= coeff[iNorm];	
     }
   }
+  //The for loop the takes this maxium value and devides 
+  //each of the coeffcients by it in order to make 
+  //them all between 1 and 0 as required
   for (jNorm=0;jNorm<=5;jNorm++){
     coeff[jNorm]=coeff[jNorm]/maxValue;
   }
-  double exp [5]={5,35,7,9,5};
-  int iOrder,jOrder;
-  int maxValue = coeff[0];
+  //////////////////// 
+  //print f statement wont work properly we need to fix it 
+  printf('The normalised array is %',poly1)
+  
+  // Return the order of the polynomial
+  
+  double exp [5]={5,35,7,9,5}; // creates an array to test the function.
+  int iOrder;//varibles forthe for loop
+  int maxValue = exp[0]; //assigns the max value to the first element
+  //the for loop below starts at the first value checks if it is larger 
+  //than the current max value if it is it rassigns this value to 
+  //maxValue other wise maxValue remains unchanged.
   for (iOrder=0;iOrder<=5;iOrder++){
     if (exp[iOrder]>maxValue){
       maxValue= exp[iOrder];	
     }
+    //when the for loop is complete the current maxValue will be the 
+    //order of the equation.
+    
   }
   
 }
